c3f4319b7f8621d581a2425e89e69ae15095f59b
Felipe Alves <felipeaamodio@gmail.com>
2023-05-17 21:40:01 -0300

    creating front end

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-5xl">Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-5xl font-bold text-zinc-50">Hello World</Text>
       <StatusBar style="light" />
     </View>
   )
